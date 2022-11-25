849c2bdb23200936037b3ae3b2c0e78d448407df
aoimaru <51444995+aoimaru@users.noreply.github.com>
2022-11-25 11:00:22 +0900

    OK

--- ABC/モノグサプログラミングコンテスト2022（AtCoder Beginner Contest 249）/C.py
+++ ABC/モノグサプログラミングコンテスト2022（AtCoder Beginner Contest 249）/C.py
@@ -1,10 +1,29 @@
 # coding: utf-8
 # Your code here!
 
+def rec(depth, memo, flag):
+    if depth == N:
+        return len([tk for tk in memo.values() if tk == K])
+    c_memo = memo.copy()
+    if flag == 1:
+        for tk in SN[depth]:
+            if tk in c_memo:
+                c_memo[tk] += 1
+            else:
+                c_memo[tk] = 1
+    return max(rec(depth+1, c_memo, 1), rec(depth+1, c_memo, 0))
+        
 def main():
+    global N, K
     N, K = map(int, input().split())
+    global SN
+    SN = list()
     for _ in range(N):
-        pass
+        SN.append(input())
+    
+    ans = max(rec(0, dict(), 1), rec(0, dict(), 0))
+    print(ans)
+
 
 if __name__ == "__main__":
     main()
